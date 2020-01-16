89439dbd32d893e37b6eb7d01fd9fd813dac099f
root <github@tryll.com>
2020-01-16 23:29:14 +0000

    hi3516d adaption

---
+++ kernel.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabihf-
+export CFLAGS="-O4 -mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard"
+export CXXFLAGS=$CFLAGS
+
+
+cd linux/linux*
+make clean
+make mrproper
+
+make hi3516d_defconfig 
+
+make -j 4
+
+make -j 4 uImage dtsb
+
+# modules
+
+
---
+++ packages/iotjs/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+
+wget -O master.zip https://github.com/jerryscript-project/iotjs/archive/master.zip
+
+
+
