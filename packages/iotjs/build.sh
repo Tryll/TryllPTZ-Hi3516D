bdc53918740520c89b655aa75a459c98d5c4438a
TRYLL Repositories <33203645+Tryll@users.noreply.github.com>
2020-01-17 23:38:25 +0100

    Update build.sh

--- packages/iotjs/build.sh
+++ packages/iotjs/build.sh
@@ -5,4 +5,9 @@
 wget -O master.zip https://github.com/jerryscript-project/iotjs/archive/master.zip
 
 
+git clone --depth 1 --single-branch https://github.com/jerryscript-project/iotjs.git
+  
+cd iotjs
+
+export CMAKE_C_COMPILER="$CROSS_COMPILE"gcc
 
