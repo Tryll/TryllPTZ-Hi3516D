42f7f460dea4b77fe9e0c9daa922dd849c30073a
root <github@tryll.com>
2020-01-18 20:55:03 +0000

    Minimal (20MB) Debian rootfs base with overlay, busybox ++

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -5,14 +5,44 @@ SRC=`dirname "$(readlink -f "$0")"`
 
 TARGET="$SRC/../target"
 
+rm -rf $TARGET
 mkdir $TARGET
 
-cd $TARGET
-mkdir etc dev proc sys usr opt var tmp mnt bin sbin usr/bin usr/sbin lib root
 
-
-
-cd lib
-cp -rd /usr/arm-linux-gnueabihf/lib/* .
-
+# Debian based rootfs with busybox udpkg.
+wget -O "$TARGET/initrd.gz" http://ftp.se.debian.org/debian/dists/stable/main/installer-armhf/current/images/cdrom/initrd.gz
  
+
+# Removing debian-installer, kernel modules and other extras
+
+cd $TARGET
+gzip -dc initrd.gz | cpio -id
+
+
+
+# CLEAN UP:
+# ***********************************************************
+rm initrd.gz
+rm -rf initrd
+
+
+# Remove debian installer stuff
+find -name '*installer*' -exec rm -rv '{}' \;
+rm -rf ./usr/lib/finish-install.d
+
+
+# Remove kernel modules
+rm -rf lib/modules/*
+
+
+# Remove installer user
+echo -e "root:*:0:0:root:/:/bin/sh" > etc/passwd
+
+
+# OVERLAY:
+#***********************************************************
+
+cp -rf $SRC/overlay/* .
+ln -s bin/busybox linuxrc
+
+
